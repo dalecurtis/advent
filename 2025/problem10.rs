a62c7f4004c62299975b39c66190df1ba2a6da4f
Dale Curtis <dalecurtis@chromium.org>
2025-12-14 14:38:28 -0800

    Add problem10 part 2 solution
    This was the first problem I wasn't sure how to solve after the
    initial astar() solution failed. It took quite a bit of time for
    me to realize its a linear programming problem and a fair bit
    more to coax good_lp() into doing what I wanted.

--- 2025/problem10.rs
+++ 2025/problem10.rs
@@ -1,6 +1,11 @@
+extern crate good_lp;
 extern crate pathfinding;
 extern crate regex;
 
+use good_lp::{
+    constraint, solvers::microlp::microlp, variable, variables, Expression, Solution, SolverModel,
+    Variable,
+};
 use regex::Regex;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -49,6 +54,69 @@ fn successors(state: &MachineState, machine: &Machine) -> Vec<(MachineState, u32
         successors.push((m, cost_m));
     }
     return successors;
+}
+
+fn solve_lp_problem(m: &Machine) -> usize {
+    // Build an buttons x joltage sized matrix.
+    // E.g. for the first sample:
+    //
+    //   [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+    //
+    // We want to solve:
+    //   | 0 0 0 1 |     |p0|
+    //   | 0 1 0 1 |     |p1|      | 3 |
+    //   | 0 0 1 0 |     |p2|      | 5 |
+    //   | 0 0 1 1 |  *  |p3]   =  | 4 |
+    //   | 1 0 1 0 |     |p4|      | 7 |
+    //   | 1 1 0 0 |     |p5|
+    //
+    // Which gives:
+    //
+    //   |  0 +  0 +  0 +  0 + p4 + p5 |     | 3 |
+    //   |  0 + p1 +  0 +  0 +  0 + p5 |  =  | 5 |
+    //   |  0 +  0 + p2 + p3 + p4 +  0 |     | 4 |
+    //   | p0 + p1 +  0 + p3 +  0 +  0 |     | 7 |
+
+    let mut matrix = vec![vec![0; m.buttons.len()]; m.joltage.len()];
+    for (i, b) in m.buttons.clone().into_iter().enumerate() {
+        for v in b {
+            matrix[v][i] = 1;
+        }
+    }
+
+    // Create variables for each button.
+    let mut vars = variables!();
+    let p: Vec<Variable> = (0..m.buttons.len())
+        .map(|_| vars.add(variable().min(0).integer()))
+        .collect();
+
+    // We want to minimize the sum of the button presses.
+    let objective: Expression = p.iter().sum();
+
+    // Use the matrix to produce
+    let mut problem = vars.minimise(objective).using(microlp);
+    for (r, j) in m.joltage.clone().into_iter().enumerate() {
+        let p_sum: Expression = matrix[r]
+            .iter()
+            .zip(p.iter())
+            .filter(|(&coeff, _)| coeff == 1)
+            .map(|(&coeff, &var)| var * coeff)
+            .sum();
+        problem = problem.with(p_sum.eq(j));
+    }
+
+    let sol = problem.solve().unwrap();
+
+    let mut total = 0;
+    for sp in p {
+        // Argh, .integer() above is not perfect.
+        let x = sol.value(sp).round();
+        if x < 0.0 {
+            todo!("Unexpected negative {}", sol.value(sp));
+        }
+        total += x as usize;
+    }
+    return total;
 }
 
 // Thanks Gemini!
@@ -109,6 +177,7 @@ fn main() {
     println!("machines.len={}", machines.len());
 
     let mut part1_count: usize = 0;
+    let mut part2_count: usize = 0;
 
     for m in machines {
         let start_state = MachineState {
@@ -117,19 +186,42 @@ fn main() {
             depth: 0,
         };
 
-        let result = prelude::astar(
-            &start_state,
-            |s: &MachineState| successors(s, &m),
-            |s: &MachineState| s.depth as u32,
-            |s: &MachineState| s.lights == m.lights,
-        );
+        {
+            let result = prelude::astar(
+                &start_state,
+                |s: &MachineState| successors(s, &m),
+                |s: &MachineState| s.depth as u32,
+                |s: &MachineState| s.lights == m.lights,
+            );
 
-        if result.is_none() {
-            todo!("unexpected no path!");
+            if result.is_none() {
+                todo!("unexpected no path!");
+            }
+            let path = result.unwrap();
+            part1_count += path.0.len() - 1; // -1 for start_state.
         }
-        let path = result.unwrap();
-        part1_count += path.0.len() - 1; // -1 for start_state.
+
+        // Too slow for part 2.
+        //
+        // {
+        //     start_state.joltage = m.joltage.clone();
+        //     let result = prelude::astar(
+        //         &start_state,
+        //         |s: &MachineState| successors2(s, &m),
+        //         |s: &MachineState| s.joltage.iter().sum::<i32>() as u32,
+        //         |s: &MachineState| s.joltage.iter().all(|&x| x == 0),
+        //     );
+        //     if result.is_none() {
+        //         todo!("unexpected no path!");
+        //     }
+        //     let path = result.unwrap();
+        //     part2_count += path.0.len() - 1; // -1 for start_state.
+        //     println!("slow={}", path.0.len() - 1);
+        // }
+
+        part2_count += solve_lp_problem(&m);
     }
 
     println!("part1 count: {}", part1_count);
+    println!("part2 count: {}", part2_count);
 }
